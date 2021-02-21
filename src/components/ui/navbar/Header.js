e595187b1bfbd6cf625528d3d965bfcdee1b59c4
Mate Hodi <matehodi@Mates-MacBook-Pro.local>
2021-02-21 11:54:34 +0100

    header

--- src/components/ui/navbar/Header.js
+++ src/components/ui/navbar/Header.js
@@ -1,31 +1,35 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+
+const createMenuItems = () => {
+    const menuName = ['campaigns', 'influencers', 'statistics']
+    return menuName.map((menu, index) => {
+        return (<Link to={menu} className='Link Header' key={menu + index}>
+            {menu.toUpperCase()}
+        </Link>)
+    })
+}
+
 export default function Header() {
     return (
-        <div className="Header">
+        <div className="HeaderContainer">
 
-            <Link to='/' className='Link'>
+
+            <Link to='/' className='Link Header Logo'>
                 StyleLike
             </Link>
 
-            <Link to='/campaings' className='Link'>
-                Campaigns
-                </Link>
-            <Link to='/influencers' className='Link'>
-                Influencers
-                </Link>
-            <Link to='/statistics' className='Link'>
-                Statistics
-            </Link>
+            <div className='MenuItems'>
+                {createMenuItems()}
+            </div>
 
-
-
-
-            Notifications
-            Image
-            Profile
-            Dropdown
+            <div className='Profile'>
+                <img height='24px'  src='/mail.svg' alt="mail" />
+                <img className="Icons" src='/profileicon.svg' alt="profile" />
+                <span style={{ paddingRight: '10px'}}>My Profile</span>
+                <img src='/dropdown.svg' alt="dropdown" />
+            </div>
         </div>
     )
 }
